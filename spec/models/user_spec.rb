5147f8959de6c0357a8f5156fb1d1c4be14d4006
Jaco Pretorius <jaco.pretorius@thoughtworks.com>
2011-12-06 18:08:12 -0500

    Adding gravatar to show user name and pic

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@
 
 require 'spec_helper'
 
-describe User do
+  describe User do
 
   before(:each) do
     @attr = { 
@@ -25,23 +25,23 @@ describe User do
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
-  
+
   it "should require a name" do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
   end
-  
+
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-  
+
   it "should reject names that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
-  
+
   it "should accept valid email addresses" do
     addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
     addresses.each do |address|
@@ -57,21 +57,21 @@ describe User do
       invalid_email_user.should_not be_valid
     end
   end  
-  
+
   it "should reject duplicate email addresses" do
     # Put a user with given email address into the database.
     User.create!(@attr)
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end  
-  
+
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-  
+
   describe "password validations" do
 
     it "should require a password" do
@@ -96,9 +96,9 @@ describe User do
       User.new(hash).should_not be_valid
     end
   end
-  
-  describe "password encryption" do
 
+  describe "password encryption" do
+    
     before(:each) do
       @user = User.create!(@attr)
     end
@@ -106,11 +106,11 @@ describe User do
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
-    
+  
     it "should set the encrypted password" do
       @user.encrypted_password.should_not be_blank
     end
-    
+  
     describe "has_password? method" do
 
       it "should be true if the passwords match" do
@@ -122,9 +122,9 @@ describe User do
       end 
     end    
   end  
-  
+
   describe "authenticate method" do
-    
+  
     before(:each) do
       @user = User.create!(@attr)
     end
